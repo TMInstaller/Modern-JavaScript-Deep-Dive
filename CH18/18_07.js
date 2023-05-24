f464a73d1ddaf50225e7d0874c2ef8188e7551fe
TMInstaller <ajvldmlqjqclr@naver.com>
2023-05-24 15:40:29 +0900

    예제 코드 실습

---
+++ CH18/18_07.js
@@ -0,0 +1,8 @@
+function sum() {
+  // arguments 객체를 배열로 변환
+  const array = Array.prototype.slice.call(arguments);
+  return array.reduce((pre, cur) => pre + cur, 0);
+}
+
+console.log(sum(1, 2)); // 3
+console.log(sum(1, 2, 3, 4, 5)); // 15
