1c8aa0e5cb7ba7c5679a83454c7ad57c417b47b6
TMInstaller <ajvldmlqjqclr@naver.com>
2023-06-27 23:28:30 +0900

    예제 코드 연습

--- CH19/19_56.js
+++ CH19/19_56.js
@@ -11,7 +11,7 @@ Person.prototype.sayHello = function () {
 // 정적 프로퍼티
 Person.staticProp = "static prop";
 
-// 정적 메서드
+// 정적 메서드.
 Person.staticMethod = function () {
   console.log("staticMethod");
 };
